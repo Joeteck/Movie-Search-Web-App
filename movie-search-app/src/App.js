c015871a91dd87f7664a7f23db51ef1d791224ee
joeteck <adeyojuibukunoluwa1@gmail.com>
2023-06-17 15:33:27 +0000

    scr Commit

--- movie-search-app/src/App.js
+++ movie-search-app/src/App.js
@@ -1,37 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { fetchMovies } from './api';
+import './App.css';
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [movies, setMovies] = useState([]);
   const [error, setError] = useState(null);
-  
+  const [totalPages, setTotalPages] = useState(1);
+
   const handleSearch = async () => {
     try {
       const data = await fetchMovies(searchTerm);
       setMovies(data);
+      setTotalPages(Math.ceil(data.totalResults / 10));
     } catch (error) {
       setError(error.message);
     }
   };
-  
+
+  useEffect(() => {
+    if (movies.length > 0) {
+      const movieList = document.querySelector('.movie-list');
+      if (movieList) {
+        movieList.style.opacity = 1;
+      }
+    }
+  }, [movies]);
   return (
-    <div>
-      <h1>Movie Search App</h1>
-      <input
-        type="text"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-      <button onClick={handleSearch}>Search</button>
+    <div className="container">
+      <h1 className="heading">Movie Search App</h1>
+      <div className="search-container">
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="search-input"
+        />
+        <button onClick={handleSearch} className="search-button">
+          Search
+        </button>
+        <select value={""} onChange={(e) => (e.target.value)}>
+          <option value="">Sort By</option>
+          <option value="rating">Rating</option>
+          <option value="release">Release Date</option>
+        </select>
+      </div>
       {error && <p>{error}</p>}
-      <ul>
+      <ul className="movie-list">
         {movies.map((movie) => (
-          <li key={movie.imdbID}>
+          <li key={movie.imdbID} className="movie-item">
             <img src={movie.Poster} alt={movie.Title} />
-            <h2>{movie.Title}</h2>
-            <p>{movie.Year}</p>
-            <button>View Details</button>
+            <div>
+              <h2>{movie.Title}</h2>
+              <p>{movie.Year}</p>
+              <button onClick={""}>
+                View Details
+              </button>
+            </div>
           </li>
         ))}
       </ul>
--- movie-search-app/src/api.js
+++ movie-search-app/src/api.js
@@ -1,14 +1,18 @@
 import axios from 'axios';
+
 export const fetchMovies = async (searchTerm) => {
-    const apiKey = 'YOUR_API_KEY_HERE';
-    const apiUrl = `http://www.omdbapi.com/?apikey=${apiKey}&s=${searchTerm}`;
-    
-    try {
-      const response = await axios.get(apiUrl);
+  const apiKey = 'a223ac01';
+  const apiUrl = `http://www.omdbapi.com/?apikey=${apiKey}&s=${searchTerm}`;
+
+  try {
+    const response = await axios.get(apiUrl);
+    if (response.status === 200) {
       return response.data.Search;
-    } catch (error) {
-      console.error(error);
-      throw new Error('Failed to fetch movie data.');
+    } else {
+      throw new Error(`Failed to fetch movie data. Code: ${response.status}`);
     }
-  };
-  
+  } catch (error) {
+    console.error(error);
+    throw new Error('Failed to fetch movie data.');
+  }
+};
