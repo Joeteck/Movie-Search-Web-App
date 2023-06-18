481a966caaffa0da7edcfe320329c6d143a20722
joeteck <adeyojuibukunoluwa1@gmail.com>
2023-06-18 01:21:05 +0000

    app commit

--- movie-search-app/src/App.js
+++ movie-search-app/src/App.js
@@ -1,31 +1,90 @@
+// App.js
 import React, { useState, useEffect } from 'react';
-import { fetchMovies } from './api';
+import { fetchMovies, fetchMovieDetails } from './api';
 import './App.css';
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [movies, setMovies] = useState([]);
   const [error, setError] = useState(null);
-  const [totalPages, setTotalPages] = useState(1);
+  const [showPopup, setShowPopup] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const [detailsVisible, setDetailsVisible] = useState(false);
+  const [sortingOption, setSortingOption] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
 
   const handleSearch = async () => {
     try {
-      const data = await fetchMovies(searchTerm);
-      setMovies(data);
-      setTotalPages(Math.ceil(data.totalResults / 10));
+      const data = await fetchMovies(searchTerm, 1);
+      console.log(data);
+      if (data && data.Search && data.Search.length > 0) {
+        setMovies(data.Search);
+        setTotalPages(Math.ceil(data.totalResults / 10));
+        setError(null);
+        setShowPopup(false);
+      } else {
+        setMovies([]);
+        setError('No movies found');
+        setShowPopup(true);
+        setTimeout(() => {
+          setShowPopup(false);
+        }, 3000);
+      }
     } catch (error) {
       setError(error.message);
     }
   };
 
-  useEffect(() => {
-    if (movies.length > 0) {
-      const movieList = document.querySelector('.movie-list');
-      if (movieList) {
-        movieList.style.opacity = 1;
+  const handleLoadMore = async () => {
+    try {
+      const nextPage = currentPage + 1;
+      const data = await fetchMovies(searchTerm, nextPage);
+      console.log(data);
+      if (data && data.Search && data.Search.length > 0) {
+        setMovies((prevMovies) => [...prevMovies, ...data.Search]);
+        setCurrentPage(nextPage);
+        setError(null);
       }
+    } catch (error) {
+      setError(error.message);
     }
-  }, [movies]);
+  };
+
+  const handleViewDetails = async (imdbID) => {
+    try {
+      const movieDetails = await fetchMovieDetails(imdbID);
+      setSelectedMovie(movieDetails);
+      setDetailsVisible(true);
+    } catch (error) {
+      console.error(error);
+      setError('Failed to fetch movie details.');
+    }
+  };
+
+  const handleCloseDetails = () => {
+    setSelectedMovie(null);
+    setDetailsVisible(false);
+  };
+
+  const handleSortBy = (option) => {
+    setSortingOption(option);
+  };
+
+  const [sortedMovies, setSortedMovies] = useState([]);
+
+  useEffect(() => {
+    let sortedMovies = [...movies];
+    if (sortingOption === 'title') {
+      sortedMovies.sort((a, b) => a.Title.localeCompare(b.Title));
+    } else if (sortingOption === 'year') {
+      sortedMovies.sort((a, b) => a.Year.localeCompare(b.Year));
+    } else if (sortingOption === 'type') {
+      sortedMovies.sort((a, b) => a.Type.localeCompare(b.Type));
+    }
+    setSortedMovies(sortedMovies);
+  }, [movies, sortingOption]);
+
   return (
     <div className="container">
       <h1 className="heading">Movie Search App</h1>
@@ -39,27 +98,57 @@ const App = () => {
         <button onClick={handleSearch} className="search-button">
           Search
         </button>
-        <select value={""} onChange={(e) => (e.target.value)}>
+        <select value={sortingOption} onChange={(e) => handleSortBy(e.target.value)}>
           <option value="">Sort By</option>
-          <option value="rating">Rating</option>
-          <option value="release">Release Date</option>
+          <option value="title">Title</option>
+          <option value="year">Year</option>
+          <option value="type">Type</option>
         </select>
       </div>
       {error && <p>{error}</p>}
+      {showPopup && (
+        <div className="popup">
+          <div className="popup-content">
+            <p>No movies found</p>
+          </div>
+        </div>
+      )}
       <ul className="movie-list">
-        {movies.map((movie) => (
+        {sortedMovies.map((movie) => (
           <li key={movie.imdbID} className="movie-item">
             <img src={movie.Poster} alt={movie.Title} />
             <div>
               <h2>{movie.Title}</h2>
               <p>{movie.Year}</p>
-              <button onClick={""}>
-                View Details
-              </button>
+              {!detailsVisible && (
+                <button onClick={() => handleViewDetails(movie.imdbID)} className="view-details-button">
+                  View Details
+                </button>
+              )}
             </div>
+            {selectedMovie && selectedMovie.imdbID === movie.imdbID && (
+              <div className="movie-details-popup">
+                <div className="movie-details">
+                  <h3>{selectedMovie.Title}</h3>
+                  <img src={selectedMovie.Poster} alt={selectedMovie.Title} className="popup-movie-poster" />
+                  <p>{selectedMovie.Plot}</p>
+                  <p>Cast: {selectedMovie.Actors}</p>
+                  <p>Genre: {selectedMovie.Genre}</p>
+                  <p>Release Date: {selectedMovie.Released}</p>
+                  <button onClick={handleCloseDetails} className="close-button">
+                    Close
+                  </button>
+                </div>
+              </div>
+            )}
           </li>
         ))}
       </ul>
+      {currentPage < totalPages && (
+        <button onClick={handleLoadMore} className="load-more-button">
+          Load More
+        </button>
+      )}
     </div>
   );
 };
--- movie-search-app/src/api.js
+++ movie-search-app/src/api.js
@@ -1,18 +1,35 @@
 import axios from 'axios';
 
-export const fetchMovies = async (searchTerm) => {
+export const fetchMovies = async (searchTerm, page) => {
   const apiKey = 'a223ac01';
-  const apiUrl = `http://www.omdbapi.com/?apikey=${apiKey}&s=${searchTerm}`;
+  const apiUrl = `http://www.omdbapi.com/?apikey=${apiKey}&s=${searchTerm}&page=${page}`;
 
   try {
     const response = await axios.get(apiUrl);
     if (response.status === 200) {
-      return response.data.Search;
+      return response.data;
     } else {
       throw new Error(`Failed to fetch movie data. Code: ${response.status}`);
     }
   } catch (error) {
     console.error(error);
     throw new Error('Failed to fetch movie data.');
+  }
+};
+
+export const fetchMovieDetails = async (imdbID) => {
+  const apiKey = 'a223ac01';
+  const apiUrl = `http://www.omdbapi.com/?apikey=${apiKey}&i=${imdbID}`;
+
+  try {
+    const response = await axios.get(apiUrl);
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      throw new Error(`Failed to fetch movie details. Code: ${response.status}`);
+    }
+  } catch (error) {
+    console.error(error);
+    throw new Error('Failed to fetch movie details.');
   }
 };
